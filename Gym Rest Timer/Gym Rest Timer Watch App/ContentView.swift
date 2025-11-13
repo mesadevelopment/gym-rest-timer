bb142796d67c0199b9c250477cb51d04e2763bc0
Lester Mesa <lester@lionskgroup.com>
2025-11-13 08:09:31 -0500

    Initial watchOS project

---
+++ Gym Rest Timer/Gym Rest Timer Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gym Rest Timer Watch App
+//
+//  Created by Lester Mesa on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gym Rest Timer/Gym Rest Timer Watch App/Gym_Rest_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_Rest_TimerApp.swift
+//  Gym Rest Timer Watch App
+//
+//  Created by Lester Mesa on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Gym_Rest_Timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
